03f93491d3ab0a17702a41b3b0039c41e66799a2
woocheolll <woocheol2868@gmail.com>
2022-10-14 13:19:23 +0900

    login

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .forms import CustomUserCreationForm
 from django.contrib.auth import login as auth_login
+from django.contrib.auth.forms import AuthenticationForm
 
 
 
@@ -10,10 +11,27 @@ def signup(request):
         if form.is_valid():
             user = form.save() # ModelForm의 save 메서드의 리턴값은 해당 모델의 인스턴스다!
             auth_login(request, user) # 로그인
-            return redirect('articles:index')
+            return redirect('accounts:index')
     else:   
         form = CustomUserCreationForm()
     context = {
         'form': form
     }
     return render(request, 'accounts/signup.html', context)
+
+def login(request):
+    if request.method == 'POST':
+        # AuthenticationForm은 ModelForm이 아님!
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            auth_login(request, form.get_user())
+            return redirect(request.GET.get('next') or 'accounts:index')
+    else:
+        form = AuthenticationForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'accounts/login.html', context)
+
+def index(request):
+    pass
